864ba11bbe692fea6beaa4ad21d4081039ef2779
freakazoid1988 <luxaeterna88@gmail.com>
2014-11-11 17:33:25 +0100

    Sixth commit! Added database, access from controller, etc.

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -6,7 +6,7 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
-        public string title { get; set; }
+        public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
